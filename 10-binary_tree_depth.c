1828f46a3be714ad918318eab0f59790103bba53
joelsteven <joelofelectronics@gmail.com>
2023-08-31 23:28:21 +0300

    Fixed null node case

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,11 +10,15 @@ size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t depth = 0;
 
+	if (node == NULL)
+		return (0);
+
+
 	while (node != NULL)
 	{
 		depth++;
 		node = node->parent;
 	}
 
-	return (depth)-1;
+	return (depth - 1);
 }
